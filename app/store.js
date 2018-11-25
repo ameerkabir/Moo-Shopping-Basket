88613dd02706c86125de0b35fdab67cc447ea77e
ameer kabir <ameerkabir2@gmail.com>
2018-11-25 23:09:42 +0000

    confugured redux dev tools

--- app/store.js
+++ app/store.js
@@ -1,14 +1,15 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/index';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/index";
 
 const config = {
-    cartApi: 'http://localhost:4567/cart'
+  cartApi: "http://localhost:4567/cart"
 };
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
-    rootReducer,
-    applyMiddleware(thunk.withExtraArgument(config))
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk.withExtraArgument(config)))
 );
 
 export default store;
