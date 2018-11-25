7f67169abc50c169177af43bf53bc41a2c4a3f60
ameer kabir <ameerkabir2@gmail.com>
2018-11-25 23:47:55 +0000

    rendered Basket component in the container

--- app/container/BasketPage.jsx
+++ app/container/BasketPage.jsx
@@ -1,49 +1,54 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import { ProductList } from '../components';
+import { ProductList,Basket } from "../components";
 
-import * as basketActions from '../actions/basket';
+import * as basketActions from "../actions/basket";
 
 export class BasketPage extends Component {
+  constructor(props) {
+    super(props);
+    props.loadBasket();
+  }
 
-    constructor(props) {
-        super(props);
-        props.loadBasket();
+  render() {
+    const { isLoading, products, basket, addToBasket } = this.props;
+
+    if (isLoading) {
+      return <div> Loading your basket </div>;
     }
 
-    render() {
-        const { isLoading, products, basket } = this.props;
-
-        if (isLoading) {
-            return <div> Loading your basket </div>;
-        }
-
-        return (
-            <div className="container">
-                <header className="mt-5 mb-5">
-                    <h1>Shopping Basket</h1>
-                </header>
-                <main className="row">
-                    <section className="col">
-                        <ProductList products={products} addToBasket={() => {}} />
-                    </section>
-                    <section className="col">
-                        {/* Basket */}
-                    </section>
-                </main>
-            </div>
-        )
-    }
-
+    return (
+      <div className="container">
+        <header className="mt-5 mb-5">
+          <h1>Shopping Basket</h1>
+        </header>
+        <main className="row">
+          <section className="col">
+            <ProductList
+              products={products}
+              itemAdded={basket.itemAdded}
+              addToBasket={addToBasket}
+            />
+          </section>
+          <section className="col">
+            <Basket basket={basket} />
+          </section>
+        </main>
+      </div>
+    );
+  }
 }
 
 export function mapStateToProps({ basket, products }) {
-    return {
-        isLoading: !basket.id,
-        products,
-        basket,
-    }
+  return {
+    isLoading: !basket.id,
+    products,
+    basket
+  };
 }
 
-export default connect(mapStateToProps, basketActions)(BasketPage);
+export default connect(
+  mapStateToProps,
+  basketActions
+)(BasketPage);
